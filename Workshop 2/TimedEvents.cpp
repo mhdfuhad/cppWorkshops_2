75c9c85b0a8dbb1bff760d8130e17dc94366367b
Mohammad Fuhad Uddin <mohammedfuhad1@gmail.com>
2021-06-07 02:56:33 +0400

    Comments updated

--- Workshop 2/TimedEvents.cpp
+++ Workshop 2/TimedEvents.cpp
@@ -3,7 +3,9 @@
 //  Workshop 2
 //
 //  Created by Mohammad Fuhad Uddin on 2021-06-06.
-//
+//  Seneca ID: 135341196
+//  Seneca Email: fmohammad15@myseneca.ca
+//  I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
 #include <iomanip>
 #include "TimedEvents.h"
--- Workshop 2/TimedEvents.h
+++ Workshop 2/TimedEvents.h
@@ -1,9 +1,12 @@
 //
-//  TimedEvents.hpp
+//  TimedEvents.h
 //  Workshop 2
 //
 //  Created by Mohammad Fuhad Uddin on 2021-06-06.
-//
+//  Seneca ID: 135341196
+//  Seneca Email: fmohammad15@myseneca.ca
+//  I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 
 #ifndef sdds_TimedEvents_h
 #define sdds_TimedEvents_h

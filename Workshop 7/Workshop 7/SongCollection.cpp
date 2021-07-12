2e51601edefb9dab4f1b52f8138625eecc17d0a7
Mohammad Fuhad Uddin <mohammedfuhad1@gmail.com>
2021-07-12 22:26:27 +0400

    WS7 completed

--- Workshop 7/Workshop 7/SongCollection.cpp
+++ Workshop 7/Workshop 7/SongCollection.cpp
@@ -8,12 +8,14 @@
 #include <iomanip>
 #include <algorithm>
 #include <fstream>
+#include <numeric>
 #include "SongCollection.h"
 
 using namespace std;
 namespace sdds{
 
 static unsigned long firstPos = 0u;
+static unsigned long countSongs = 0u;
 
 SongCollection::SongCollection(string fileName){
     ifstream fin(fileName);
@@ -41,7 +43,7 @@ SongCollection::SongCollection(string fileName){
             
             temp2 = getString(temp, 5);
             removeSpaces(temp2);
-            track.m_Price = stod(temp2);
+            track.m_price = stod(temp2);
             
             collection.push_back(track);
             
@@ -53,10 +55,56 @@ SongCollection::SongCollection(string fileName){
     }
 };
 
+void SongCollection::sort(string fieldName){
+    std::sort(collection.begin(), collection.end(), [fieldName](const Song& obj, const Song& obj2){
+        if(fieldName == "title")
+            return obj.m_Title < obj2.m_Title;
+        else if(fieldName == "album")
+            return obj.m_Album < obj2.m_Album;
+        else if(fieldName == "length")
+            return obj.m_Length < obj2.m_Length;
+        else
+            return false;
+    });
+};
+
+void SongCollection::cleanAlbum(){
+    for_each(collection.begin(), collection.end(), [](Song& obj){
+        if(obj.m_Album == "[None]")
+            obj.m_Album = "";
+        else
+            obj.m_Album = obj.m_Album;
+    });
+};
+
+bool SongCollection::inCollection(string artist) const{
+    countSongs = count_if(collection.begin(), collection.end(), [artist](const Song& obj){
+        return artist == obj.m_Artist;
+    });
+    return countSongs > 0;
+};
+
+list<Song> SongCollection::getSongsForArtist(string artist) const{
+    list<Song> trackList(countSongs);
+    copy_if(collection.begin(), collection.end(), trackList.begin(), [artist](const Song& obj){
+        return obj.m_Artist == artist;
+    });
+    return trackList;
+};
+
 void SongCollection::display(ostream& out) const{
     for_each(collection.begin(), collection.end(), [&out](const Song& theSong){
         out << theSong << endl;
     });
+    
+    size_t sum = accumulate(collection.begin(), collection.end(), 0, [](size_t total, const Song& song){
+        return total += song.m_Length;
+    });
+    
+    out << setw(89) << setfill('-') << '\n' << setfill(' ');
+    out << "| " << setw(77) << right << "Total Listening Time: ";
+    out << sum/3600 << ":" << (sum - ((sum/3600) * 3600))/60 << ":" << sum % 60 <<  " |" << endl;
+    
 };
 
 ostream& operator<<(ostream& out, const Song& theSong){
@@ -70,7 +118,7 @@ ostream& operator<<(ostream& out, const Song& theSong){
         out << ' ';
     out << " | " << theSong.m_Length / 60 << ":";
     out << setw(2) << setfill('0') << theSong.m_Length % 60 << " | " << setfill(' ');
-    out << setw(3) << setprecision(2) << fixed << theSong.m_Price << " |";
+    out << setw(3) << setprecision(2) << fixed << theSong.m_price << " |";
     return out;
 };
 
--- Workshop 7/Workshop 7/SongCollection.h
+++ Workshop 7/Workshop 7/SongCollection.h
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <vector>
+#include <list>
 
 namespace sdds{
 
@@ -17,7 +18,7 @@ struct Song{
     std::string m_Artist{};
     std::string m_Title{};
     std::string m_Album{};
-    double m_Price{0.0};
+    double m_price{0.0};
     size_t m_Year{0};
     size_t m_Length{0};
 };
@@ -29,6 +30,10 @@ class SongCollection {
 public:
     SongCollection(std::string fileName);
     void display(std::ostream& out) const;
+    void sort(std::string fieldName);
+    void cleanAlbum();
+    bool inCollection(std::string artist) const;
+    std::list<Song> getSongsForArtist(std::string artist) const;
 };
 std::ostream& operator<<(std::ostream& out, const Song& theSong);
 
